0d06fc2719b3c95e0172182bd688b57d6bcde334
ganlvtech <ganlvtech@qq.com>
2017-06-02 20:37:37 +0800

    修改配置，使用env文件

--- application/database.php
+++ application/database.php
@@ -9,17 +9,19 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
+use think\Env;
+
 return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'down-2017-06',
+    'database'        => Env::get('database.database', ''),
     // 用户名
-    'username'        => 'down-2017-06',
+    'username'        => Env::get('database.username', 'root'),
     // 密码
-    'password'        => '',
+    'password'        => Env::get('database.password', ''),
     // 端口
     'hostport'        => '',
     // 连接dsn
---
+++ application/extra/filesystem.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'root' => ROOT_PATH . 'public', // 服务器根目录
+    'down' => ROOT_PATH . 'public/upload/down', // 软件文件夹
+    'icon' => ROOT_PATH . 'public/upload/icon', // 图标文件夹
+];
