f9d534b92f72ac0948cebeca751efa02fe230a08
ganlvtech <ganlvtech@qq.com>
2017-06-08 03:56:19 +0800

    代码调整

--- application/extra/filesystem.php
+++ application/extra/filesystem.php
@@ -2,6 +2,6 @@
 
 return [
     'root' => ROOT_PATH . 'public', // 服务器根目录
-    'down' => ROOT_PATH . 'public/upload/down', // 软件文件夹
-    'icon' => ROOT_PATH . 'public/upload/icon', // 图标文件夹
+    'down' => ROOT_PATH . 'public' . DS . 'upload' . DS . 'down', // 软件文件夹
+    'icon' => ROOT_PATH . 'public' . DS . 'upload' . DS . 'icon', // 图标文件夹
 ];
